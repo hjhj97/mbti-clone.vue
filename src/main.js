4cefff14396bb5332c2a085ee8a587a80f602dfc
JuHeonHa <hjhjaueon97@naver.com>
2022-12-10 17:40:31 +0900

    정리

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import stores from './stores';
 import router from './router';
+import axios from '@/plugins/axios';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.config.globalProperties.axios = axios;
+
+app.use(stores).use(router).mount('#app');
